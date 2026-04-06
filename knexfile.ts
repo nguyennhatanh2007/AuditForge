50fccef42862ab774e705a7808004525f1a5ee84
AuditForge Dev <dev@auditforge.local>
2026-04-06 08:50:21 +0700

    chore(php-version): remove Node.js stack files

--- knexfile.ts
+++
@@ -1,5 +0,0 @@
-import { getMysqlConfig } from './src/config/mysql';
-
-const config = getMysqlConfig();
-
-export default config;
--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-	serverExternalPackages: ['knex', 'mysql2'],
-};
-
-export default nextConfig;
